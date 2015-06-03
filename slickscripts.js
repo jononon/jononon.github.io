4e0847657ffa097422e4bc9e33493e93b5db6b67
Jonathan Damico <jj_damico@yahoo.com>
2015-06-02 18:27:03 -0700

    oooh i figured it out

---
+++ slickscripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $('.titlescroll').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        fade: false,
+        pauseOnHover: false,
+        slidesToShow: 1,
+        variableWidth: false,
+    });
+});
