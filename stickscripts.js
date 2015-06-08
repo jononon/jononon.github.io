1e7e2d8d82ce9ad3b45e9e5aa64fa1d520a91cc8
Jonathan Damico <jj_damico@yahoo.com>
2015-06-08 09:44:25 -0700

    testing stick

--- stickscripts.js
+++ stickscripts.js
@@ -1,5 +1,5 @@
 var options = {
-  top: 0, // This represents the buffer you want between the top of the page and the div in px. Default is 0.
+  top: 100, // This represents the buffer you want between the top of the page and the div in px. Default is 0.
   min_screen_width: 0, // This represents the minimum screen width that the stickiness will be applied to. Default is 0.
   bottom: 0, // This represents the buffer you want between the bottom of the page and the div in px. Default is 0.
   outer_div: 300 // This is a jquery or DOM element whose height needs to be greater than the div in order to make it sticky. Default is null.
