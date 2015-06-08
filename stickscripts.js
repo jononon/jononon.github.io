1e1587ba3c7a99800f811bfa2ab2b459d0a9c6b6
Jonathan Damico <jj_damico@yahoo.com>
2015-06-08 09:48:00 -0700

    sticky
    please work

--- stickscripts.js
+++ stickscripts.js
@@ -1,7 +1,14 @@
-var options = {
-  top: 100, // This represents the buffer you want between the top of the page and the div in px. Default is 0.
-  min_screen_width: 0, // This represents the minimum screen width that the stickiness will be applied to. Default is 0.
-  bottom: 0, // This represents the buffer you want between the bottom of the page and the div in px. Default is 0.
-  outer_div: 300 // This is a jquery or DOM element whose height needs to be greater than the div in order to make it sticky. Default is null.
-};
-$('#my_div').sticky_div(options);
+function sticky_relocate() {
+    var window_top = $(window).scrollTop();
+    var div_top = $('#sticky-anchor').offset().top;
+    if (window_top > div_top) {
+        $('#sticky').addClass('stick');
+    } else {
+        $('#sticky').removeClass('stick');
+    }
+}
+
+$(function () {
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+});
